a076686ce2607e1694477c2d8610cbf9d1944730
Zalina <zalina_avarova@mail.ru>
2022-08-22 16:22:17 +0700

    Домашнее задание

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿//Задача №2
+//int a = 5;
+//int b = 7;
+//int max = a;
+//if (b > max) max = b;
+//Console.WriteLine(max);
+
+//int a = 2;
+//int b = 10;
+//int max = a;
+//if (b > max) max = b;
+//Console.WriteLine(max);
+
+//int a = -9;
+//int b = -3;
+//int max = a;
+//if (b > max) max = b;
+//Console.WriteLine(max);
+
+
+//Задача №4
+
+//int a = 2;
+//int b = 3;
+//int c = 7;
+//int max = a;
+//if (b > max) max = b;
+//if (c > max) max = c;
+//Console.WriteLine(max);
+
+
+
+//int a = 44;
+//int b = 5;
+//int c = 78;
+//int max = a;
+//if (b > max) max = b;
+//if (c > max) max = c;
+//Console.WriteLine(max);
+
+//int a = 22;
+//int b = 3;
+//int c = 9;
+//int max = a;
+//if (b > max) max = b;
+//if (c > max) max = c;
+//Console.WriteLine(max);
+
+//Задача №6
+
+//Console.WriteLine("Введите число: ");
+//int a = int.Parse(Console.ReadLine());
+//if (a % 2 == 1)
+//{
+    //Console.WriteLine("Число нечётное");
+//}
+//else
+//{
+    // Console.WriteLine("Число чётное");
+//}
+
+
+//Задача №8
+
+Console.WriteLine("Введите длину: ");
+int n = int.Parse(Console.ReadLine());
+for (int a = 1; a <= n; a++)
+if (a % 2 == 0)
+
+{
+    Console.WriteLine("Числа четные");
+}
+
+
+
